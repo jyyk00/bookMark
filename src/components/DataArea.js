7bcf1a3cb03fe52308f2b6bb4afaba32c2b829cd
programmerkim <jenniferkim.businessonly@gmail.com>
2020-03-28 22:36:00 -0700

    imports in dataarea

---
+++ src/components/DataArea.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import DataTable from "./DataTable";
+import Nav from "./Nav";
+import API from "../utils/API";
+import "../styles/DataArea.css";
+import DataAreaContext from "../utils/DataAreaContext";
