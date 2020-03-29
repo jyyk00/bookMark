f9a78686804dbecd02e762a3435b802766df3c81
programmerkim <jenniferkim.businessonly@gmail.com>
2020-03-29 15:02:01 -0700

    setting up const

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -3,3 +3,7 @@ import React, { useContext } from "react";
 import DataBody from "./DataBody";
 import "../styles/DataTable.css";
 import DataAreaContext from "../utils/DataAreaContext";
+
+const DataTable = () => {
+    const context = useContext(DataAreaContext);
+  
