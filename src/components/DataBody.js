138497237ed8cca17fb1e28b5c66b11fdb5fb6e2
programmerkim <jenniferkim.businessonly@gmail.com>
2020-03-28 22:46:14 -0700

    databody

--- src/components/DataBody.js
+++ src/components/DataBody.js
@@ -1,3 +1,6 @@
 import React, { useContext } from "react";
 import "../styles/DataBody.css";
 import DataAreaContext from "../utils/DataAreaContext";
+
+const DataBody = () => {
+    const context = useContext(DataAreaContext);
