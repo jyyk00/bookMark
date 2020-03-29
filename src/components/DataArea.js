f8b6bd418e84f15f2262bafde0171385a89c565e
programmerkim <jenniferkim.businessonly@gmail.com>
2020-03-28 22:41:25 -0700

    const

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -7,60 +7,65 @@ import DataAreaContext from "../utils/DataAreaContext";
 
 const DataArea = () => {
     const [developerState, setDeveloperState] = useState({
-    users: [],
-    order: "descend",
-    filteredUsers: [],
-    headings: [
-      { name: "Image", width: "10%", order: "descend" },
-      { name: "name", width: "10%", order: "descend" },
-      { name: "phone", width: "20%", order: "descend" },
-      { name: "email", width: "20%", order: "descend" },
-      { name: "dob", width: "10%", order: "descend" }
-    ]
-  });
+        users: [],
+        order: "descend",
+        filteredUsers: [],
+        headings: [
+            { name: "Image", width: "10%", order: "descend" },
+            { name: "name", width: "10%", order: "descend" },
+            { name: "phone", width: "20%", order: "descend" },
+            { name: "email", width: "20%", order: "descend" },
+            { name: "dob", width: "10%", order: "descend" }
+        ]
+    });
 
-  const handleSort = heading => {
-    let currentOrder = developerState.headings
-    .filter(elem => elem.name === heading)
-      .map(elem => elem.order)
-      .toString();
+    const handleSort = heading => {
+        let currentOrder = developerState.headings
+            .filter(elem => elem.name === heading)
+            .map(elem => elem.order)
+            .toString();
 
-    if (currentOrder === "descend") {
-      currentOrder = "ascend";
-    } else {
-      currentOrder = "descend";
-    }
-
-    const compareFnc = (a, b) => {
-        if (currentOrder === "ascend") {
-          // account for missing values
-          if (a[heading] === undefined) {
-            return 1;
-          } else if (b[heading] === undefined) {
-            return -1;
-          }
-           // numerically
-        else if (heading === "name") {
-            return a[heading].first.localeCompare(b[heading].first);
-          } else if (heading === "dob") {
-            return a[heading].age - b[heading].age;
-          } else {
-            return a[heading].localeCompare(b[heading]);
-          }
+        if (currentOrder === "descend") {
+            currentOrder = "ascend";
         } else {
-          // account for missing values
-          if (a[heading] === undefined) {
-            return 1;
-          } else if (b[heading] === undefined) {
-            return -1;
-          }
-           // numerically
-        else if (heading === "name") {
-            return b[heading].first.localeCompare(a[heading].first);
-          }else if (heading === "dob") {
-            return b[heading].age - a[heading].age;
-          }  else {
-            return b[heading].localeCompare(a[heading]);
-          }
+            currentOrder = "descend";
         }
-      };
+
+        const compareFnc = (a, b) => {
+            if (currentOrder === "ascend") {
+                // account for missing values
+                if (a[heading] === undefined) {
+                    return 1;
+                } else if (b[heading] === undefined) {
+                    return -1;
+                }
+                // numerically
+                else if (heading === "name") {
+                    return a[heading].first.localeCompare(b[heading].first);
+                } else if (heading === "dob") {
+                    return a[heading].age - b[heading].age;
+                } else {
+                    return a[heading].localeCompare(b[heading]);
+                }
+            } else {
+                // account for missing values
+                if (a[heading] === undefined) {
+                    return 1;
+                } else if (b[heading] === undefined) {
+                    return -1;
+                }
+                // numerically
+                else if (heading === "name") {
+                    return b[heading].first.localeCompare(a[heading].first);
+                } else if (heading === "dob") {
+                    return b[heading].age - a[heading].age;
+                } else {
+                    return b[heading].localeCompare(a[heading]);
+                }
+            }
+        };
+        const sortedUsers = developerState.filteredUsers.sort(compareFnc);
+        const updatedHeadings = developerState.headings.map(elem => {
+            elem.order = elem.name === heading ? currentOrder : elem.order;
+            return elem;
+        });
