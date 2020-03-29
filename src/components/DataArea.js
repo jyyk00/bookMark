f7241dd67a85ea4097373a3fe22d2b3735f7131e
programmerkim <jenniferkim.businessonly@gmail.com>
2020-03-28 22:40:33 -0700

    compare func finish

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -54,3 +54,13 @@ const DataArea = () => {
           } else if (b[heading] === undefined) {
             return -1;
           }
+           // numerically
+        else if (heading === "name") {
+            return b[heading].first.localeCompare(a[heading].first);
+          }else if (heading === "dob") {
+            return b[heading].age - a[heading].age;
+          }  else {
+            return b[heading].localeCompare(a[heading]);
+          }
+        }
+      };
