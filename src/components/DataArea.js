11bac1a955a007f647e8324d6fbea013c49684f8
programmerkim <jenniferkim.businessonly@gmail.com>
2020-03-28 22:37:21 -0700

    const func

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -7,3 +7,14 @@ import DataAreaContext from "../utils/DataAreaContext";
 
 const DataArea = () => {
     const [developerState, setDeveloperState] = useState({
+    users: [],
+    order: "descend",
+    filteredUsers: [],
+    headings: [
+      { name: "Image", width: "10%", order: "descend" },
+      { name: "name", width: "10%", order: "descend" },
+      { name: "phone", width: "20%", order: "descend" },
+      { name: "email", width: "20%", order: "descend" },
+      { name: "dob", width: "10%", order: "descend" }
+    ]
+  });
